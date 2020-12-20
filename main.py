8e88867e573bdf11bae8ac7d2c1dda437f6bdc6b
maxoja <tawan.tpptnk@gmail.com>
2020-12-20 21:27:26 +0000

    Add todos

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from fx_findings import spread_analysis
 
 reversal_analysis.run()
 # spread_analysis.run()
+
+# TODO
+'''
+- make analysis configurable beautifully
+- decompose funtions in reversal_analysis
+- advance analysis to find best TP SL
+- iteration over different analysis settings
+'''
