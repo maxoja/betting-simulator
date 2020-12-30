9e0d7b97da239ee34c2c16b8adda655c17b686ea
maxoja <tawan.tpptnk@gmail.com>
2020-12-30 00:32:19 +0000

    Remove unused code

--- fx_findings/base/plotting.py
+++ fx_findings/base/plotting.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 
 
-def plot_histogram_unblock(sample_data):
-    plt.figure()
+def plot_histogram_unblock(sample_data, title=""):
+    plt.figure(title)
     plt.hist(sample_data, bins=128)
     plt.show(block=False)
 
 
-def plot_centered_cumulative_histogram(sample_data, center_val=0):
+def plot_centered_cumulative_histogram(sample_data, center_val=0, title=""):
     if not sample_data:
-        plt.figure()
+        plt.figure(title)
         plt.plot([])
         plt.show(block=False)
         return
@@ -17,7 +17,7 @@ def plot_centered_cumulative_histogram(sample_data, center_val=0):
     mean_index = min(range(len(sample_data)), key=lambda i: abs(center_val-sample_data[i]))
     count_y = [abs(mean_index-i)/len(sample_data)*100 for i,val in enumerate(sample_data)]
     count_x = sample_data[::]
-    plt.figure()
+    plt.figure(title)
     plt.plot(count_x, count_y)
     plt.show(block=False)
 
--- fx_findings/reversal_analysis/analyse.py
+++ fx_findings/reversal_analysis/analyse.py
@@ -45,51 +45,6 @@ def entry_points_rsi_reversal(df, settings:Settings, irange:utils.IndexRange=Non
             entries.append(utils.Index(irange, i), PosType.SHORT)
 
     return entries
-
-# def analyse_rsi_reversal(df, settings:Settings, plot=True):
-    
-#     close_series = df[Col.CLOSE].reset_index(drop=True)
-#     rsi_series = talib.RSI(close_series, settings.rsi_period)
-    
-#     short_diff = []
-#     long_diff = []
-#     for i in range(len(rsi_series)):
-#         current_rsi = rsi_series[i]
-
-#         if numpy.isnan(current_rsi):
-#             continue
-#         if len(rsi_series) - i <= settings.future_window:
-#             continue
-
-#         current_close = close_series[i]
-#         future_close = close_series[i+settings.future_window]
-#         future_change_points = (future_close - current_close)/utils.point_size(settings.quote)
-
-#         if current_rsi >= 100-settings.padding_thresh:
-#             short_diff.append(future_change_points)
-#         if current_rsi <= settings.padding_thresh:
-#             long_diff.append(future_change_points)
-
-#     n_bars = len(close_series)-settings.rsi_period
-#     n_years = n_bars/utils.annual_bars(settings.timeframe)
-#     total_short = len(short_diff)
-#     total_long = len(long_diff)
-
-#     print(settings.as_str())
-#     print('# Bars', n_bars, f'# Years {n_years:.2f}')
-#     if total_short > 0:
-#         print('overbought', total_short, utils.avg(short_diff))
-#     if total_long > 0:
-#         print('oversold  ', total_long, utils.avg(long_diff))
-#     if total_short > 0 and total_long > 0:
-#         print('weighted  ', total_short+total_long, utils.avg(list(map(lambda x: -x, short_diff)) + long_diff) )
-    
-#     if plot:
-#         plotting.plot_histogram_unblock(short_diff)
-#         plotting.plot_histogram_unblock(long_diff)
-#         plotting.plot_centered_cumulative_histogram(short_diff)
-#         plotting.plot_centered_cumulative_histogram(long_diff)
-#         plotting.show_plot()
     
 def analyse_position_progression(df, entries:utils.EntryIndices, win_size=1, plot=True):
     close = df[Col.CLOSE]
@@ -123,23 +78,20 @@ def analyse_position_progression(df, entries:utils.EntryIndices, win_size=1, plo
         print('weighted  ', total_short+total_long, utils.avg(list(map(lambda x: -x, short_diff)) + long_diff) )
     
     if plot:
-        plotting.plot_histogram_unblock(short_diff)
-        plotting.plot_histogram_unblock(long_diff)
-        plotting.plot_centered_cumulative_histogram(short_diff)
-        plotting.plot_centered_cumulative_histogram(long_diff)
+        plotting.plot_histogram_unblock(short_diff, title="short prof/loss")
+        plotting.plot_histogram_unblock(long_diff, title="long prof/loss")
+        plotting.plot_centered_cumulative_histogram(short_diff, title="accumulated hist for short positions")
+        plotting.plot_centered_cumulative_histogram(long_diff, title="accumulated hist for long positions")
         plotting.show_plot()
 
 def run():
     PARAM_PERIOD = [14]
-    # PARAM_FUTURE_WIN = [1,2,3]
     PARAM_PADDING = [26,28,30,32,34]
     
     for period in PARAM_PERIOD:
         for padding in PARAM_PADDING:
-        # for future_win in PARAM_FUTURE_WIN:
             settings = Settings(Quote.AUDCAD, Timeframe.D1, period, padding)
             df = loader.load(settings.timeframe, settings.quote)
             entries = entry_points_rsi_reversal(df, settings, None)
             analyse_position_progression(df, entries)
-            # analyse_rsi_reversal(df, settings, plot=False)
             print()
