af5416e7618b2651fbf8472282948f2e17fda828
maxoja <tawan.tpptnk@gmail.com>
2021-01-07 20:12:08 +0000

    Add caching to data frame loader

--- fx_findings/base/loader.py
+++ fx_findings/base/loader.py
@@ -2,7 +2,18 @@ import os
 import pandas
 from .enums import Timeframe, Quote, Broker
 
+cache = dict()
+
 def load(timeframe:Timeframe, quote:Quote, broker:Broker=None):
+    cache_key = (timeframe, quote, broker)
+
+    if cache_key in cache:
+        print('use cache')
+        if cache[cache_key] is None:
+            return None
+        else:
+            return cache[cache_key].copy()
+    
     directory = f'data/{timeframe}/{quote}'
 
     if not broker is None:
@@ -15,6 +26,8 @@ def load(timeframe:Timeframe, quote:Quote, broker:Broker=None):
                 dataframe = pandas.read_csv(filepath, sep='\t', parse_dates={'<DATETIME>':[0]}, infer_datetime_format=True)
             else:
                 dataframe = pandas.read_csv(filepath, sep='\t', parse_dates={'<DATETIME>':[0,1]}, infer_datetime_format=True)
+            cache[cache_key] = dataframe.copy()
             return dataframe
 
+    cache[cache_key] = None
     return None
