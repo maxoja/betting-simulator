ec3213531264d66be605673b63d5d307d171b728
maxoja <tawan.tpptnk@gmail.com>
2021-03-22 22:33:00 +0000

    Refact: Simplify quote point size function

--- fx_findings/base/utils/market.py
+++ fx_findings/base/utils/market.py
@@ -1,36 +1,5 @@
 from ..enums import Quote, Col, Timeframe
 
-__point_size_of = {
-    Quote.AUDCAD: 0.00001,
-    Quote.AUDCHF: 0.00001,
-    Quote.AUDJPY: 0.001,
-    Quote.AUDNZD: 0.00001,
-    Quote.AUDUSD: 0.00001,
-    Quote.CADCHF: 0.00001,
-    Quote.CADJPY: 0.001,
-    Quote.CHFJPY: 0.001,
-    Quote.EURAUD: 0.00001,
-    Quote.EURCAD: 0.00001,
-    Quote.EURCHF: 0.00001,
-    Quote.EURGBP: 0.00001,
-    Quote.EURJPY: 0.001,
-    Quote.EURNZD: 0.00001,
-    Quote.EURUSD: 0.00001,
-    Quote.GBPAUD: 0.00001,
-    Quote.GBPCAD: 0.00001,
-    Quote.GBPCHF: 0.00001,
-    Quote.GBPJPY: 0.001,
-    Quote.GBPNZD: 0.00001,
-    Quote.GBPUSD: 0.00001,
-    Quote.NZDCAD: 0.00001,
-    Quote.NZDCHF: 0.00001,
-    Quote.NZDJPY: 0.001,
-    Quote.NZDUSD: 0.00001,
-    Quote.USDCAD: 0.00001,
-    Quote.USDCHF: 0.00001,
-    Quote.USDJPY: 0.001
-}
-
 __annual_bars_of = {
     Timeframe.D1: 261,
     Timeframe.H4: 261*6,
@@ -45,7 +14,9 @@ def average_spread(df):
 
 
 def point_size(quote:Quote):
-    return __point_size_of[quote]
+    if 'JPY' in (''+quote):
+        return 0.001
+    return 0.00001
 
 
 def pip_size(quote:Quote):
--- main_dev.py
+++ main_dev.py
@@ -1,5 +1,6 @@
 from pandas import DataFrame
 from fx_findings.base import utils
+from fx_findings.base.enums import Quote
 
 data = {'number': [i for i in range(1,11)]}
 df = DataFrame(data)
@@ -14,4 +15,9 @@ print()
 
 print('slice from back\nsize=2, shift=3, buffer=1')
 print(utils.pandas.slice_frame_from_front(df, 2, 3, 1))
+print()
+
+print(utils.market.point_size(Quote.AUDJPY))
+print(utils.market.point_size(Quote.EURJPY))
+print(utils.market.point_size(Quote.AUDNZD))
 print()
